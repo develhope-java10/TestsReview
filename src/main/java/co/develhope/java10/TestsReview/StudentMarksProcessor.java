ff8e17cc2a383cd8111d0fc31b4e557da2f5c4c1
federico urbani <urbanifederico5@gmail.com>
2023-05-23 15:01:50 +0200

    typos in studentmarksprocessor

--- src/main/java/co/develhope/java10/TestsReview/StudentMarksProcessor.java
+++ src/main/java/co/develhope/java10/TestsReview/StudentMarksProcessor.java
@@ -7,15 +7,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class StudentMarksProcessor {
     public double getClassroomMarks(List<Student> classroomStudents) {
-        if (classroomStudents.size()==0) return 0;
-        double sum=0;
-        for (Student s: classroomStudents){
-            if (s==null){
+        if (classroomStudents.size() == 0) return 0;
+        double sum = 0;
+        for (Student s : classroomStudents) {
+            if (s == null) {
                 throw new NullPointerException("stud is null");
             }
-            sum+=s.getAverageMarks();
+            sum += s.getAverageMarks();
         }
-        double average=sum/classroomStudents.size();
+        double average = sum / classroomStudents.size();
         return average;
     }
 }
